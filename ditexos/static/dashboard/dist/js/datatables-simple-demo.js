bde1a618d4939b9a13f39985e25123f7bec0590b
Maxim Myasischev <mnmyasis@gmail.com>
2021-11-10 18:28:48 +0300

    v1.1

---
+++ ditexos/static/dashboard/dist/js/datatables-simple-demo.js
@@ -0,0 +1,39 @@
+window.addEventListener('DOMContentLoaded', event => {
+    // Simple-DataTables
+    // https://github.com/fiduswriter/Simple-DataTables/wiki
+
+    const datatablesSimple = document.getElementById('datatablesSimple');
+    const datatablesSimple2 = document.getElementById('datatablesSimple2');
+    const datatables = document.getElementsByClassName('datatable')
+    const mini_table = document.getElementsByClassName('mini-table')
+    const period_table = document.getElementsByClassName('period-table')
+    if (datatablesSimple ) {
+        new simpleDatatables.DataTable(datatablesSimple);
+    }
+    if (datatablesSimple2 ) {
+        new simpleDatatables.DataTable(datatablesSimple2);
+    }
+    if (datatables.length > 0){
+        for(i = 0; i < datatables.length; i++){
+             s = new simpleDatatables.DataTable(datatables[i], {
+             })
+        }
+    }
+    if (mini_table.length > 0){
+        for(i = 0; i < mini_table.length; i++){
+            new simpleDatatables.DataTable(mini_table[i], {
+                searchable: false,
+                paging: false,
+                info: false
+            });
+        }
+    }
+    if (period_table.length > 0){
+        for(i = 0; i < period_table.length; i++){
+            new simpleDatatables.DataTable(period_table[i], {
+                searchable: false
+            });
+        }
+    }
+
+});
