3e26ff2cc8b491864465d5ea10d08de0a3cabc82
Maxim Myasischev <mnmyasis@gmail.com>
2021-11-23 03:00:01 +0300

    cahnge excel export

--- ditexos/dashboard/views.py
+++ ditexos/dashboard/views.py
@@ -1,5 +1,5 @@
 from io import BytesIO
-
+import xlsxwriter
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import HttpResponse
 from django.shortcuts import redirect
@@ -135,41 +135,281 @@ class ClientReportDetailView(LoginRequiredMixin, DetailView):
             context['p2_end_date'] = datetime.datetime.now().strftime('%Y-%m-%d')
         return context
 
+    def export_excel_period(self, workbook, worksheet, items, title, start_row=3, skip_row=3):
+        col = 0
+        if len(items) > 0:
+            keys = items[0].keys()
+        else:
+            ValueError('list not items')
+        columns_name = ['Campaign', 'Period', 'Cost', 'Impressions', '	Clicks', 'CTR', 'CPC', 'CR', 'CPL', 'Leads']
+        width = len(columns_name)
+        title_format = workbook.add_format({
+            'bold': False,
+            'font_color': 'black',
+            'border': True,
+            'bg_color': 'white',
+            'align': 'center_across',
+            'font_size': 24,
+            'font_name': 'calibri'
+        }
+        )
+
+        worksheet.merge_range(start_row - 2, col, start_row - 1, col + 4, title, title_format)
+        start_row += 2
+        for column_name in columns_name:
+            cell_format = workbook.add_format({
+                'bold': True,
+                'font_color': 'black',
+                'border': True,
+                'bg_color': 'd7f2f5',
+                'align': 'center_across',
+                'font_size': 12,
+                'font_name': 'calibri'
+                }
+            )
+            worksheet.write(start_row, col, column_name, cell_format)
+            col += 1
+        start_row += 1
+        for item in items:
+            col = 0
+            cell_format = workbook.add_format({
+                'bold': False,
+                'font_color': 'black',
+                'border': True,
+                'bg_color': 'white',
+                'align': 'center_across',
+                'font_size': 8,
+                'font_name': 'calibri'
+                }
+            )
+            cell_format_warning = workbook.add_format({
+                'bold': False,
+                'font_color': 'red',
+                'border': True,
+                'bg_color': 'd7f5e1',
+                'align': 'center_across',
+                'font_size': 8,
+                'font_name': 'calibri'
+                }
+            )
+            cell_format_total = workbook.add_format({
+                'bold': False,
+                'font_color': 'black',
+                'border': True,
+                'bg_color': 'd7f5e1',
+                'align': 'center_across',
+                'font_size': 8,
+                'font_name': 'calibri'
+                }
+            )
+
+            worksheet.merge_range(f'A{start_row + 1}:A{start_row + 2}', item.get('campaign'), cell_format)
+            col += 1
+            worksheet.write(start_row, col, item.get('p1'), cell_format)
+            worksheet.write(start_row + 1, col, item.get('p2'), cell_format)
+            col += 1
+            worksheet.write(start_row, col, item.get('p1_cost_'), cell_format)
+            worksheet.write(start_row + 1, col, item.get('p2_cost_'), cell_format)
+            if item.get('change_cost_') < 0:
+                worksheet.write(start_row + 2, col, item.get('change_cost_'), cell_format_warning)
+            else:
+                worksheet.write(start_row + 2, col, item.get('change_cost_'), cell_format_total)
+            col += 1
+            worksheet.write(start_row, col, item.get('p1_impressions'), cell_format)
+            worksheet.write(start_row + 1, col, item.get('p2_impressions'), cell_format)
+            if item.get('change_impressions') < 0:
+                worksheet.write(start_row + 2, col, item.get('change_impressions'), cell_format_warning)
+            else:
+                worksheet.write(start_row + 2, col, item.get('change_impressions'), cell_format_total)
+            col += 1
+            worksheet.write(start_row, col, item.get('p1_clicks'), cell_format)
+            worksheet.write(start_row + 1, col, item.get('p2_clicks'), cell_format)
+            if item.get('change_clicks') < 0:
+                worksheet.write(start_row + 2, col, item.get('change_clicks'), cell_format_warning)
+            else:
+                worksheet.write(start_row + 2, col, item.get('change_clicks'), cell_format_total)
+            col += 1
+            worksheet.write(start_row, col, item.get('p1_ctr'), cell_format)
+            worksheet.write(start_row + 1, col, item.get('p2_ctr'), cell_format)
+            if item.get('change_ctr') < 0:
+                worksheet.write(start_row + 2, col, item.get('change_ctr'), cell_format_warning)
+            else:
+                worksheet.write(start_row + 2, col, item.get('change_ctr'), cell_format_total)
+            col += 1
+            worksheet.write(start_row, col, item.get('p1_cpc'), cell_format)
+            worksheet.write(start_row + 1, col, item.get('p2_cpc'), cell_format)
+            if item.get('change_cpc') < 0:
+                worksheet.write(start_row + 2, col, item.get('change_cpc'), cell_format_warning)
+            else:
+                worksheet.write(start_row + 2, col, item.get('change_cpc'), cell_format_total)
+            col += 1
+            worksheet.write(start_row, col, item.get('p1_cr'), cell_format)
+            worksheet.write(start_row + 1, col, item.get('p2_cr'), cell_format)
+            if item.get('change_cr') < 0:
+                worksheet.write(start_row + 2, col, item.get('change_cr'), cell_format_warning)
+            else:
+                worksheet.write(start_row + 2, col, item.get('change_cr'), cell_format_total)
+            col += 1
+            worksheet.write(start_row, col, item.get('p1_cpl'), cell_format)
+            worksheet.write(start_row + 1, col, item.get('p2_cpl'), cell_format)
+            if item.get('change_cpl') < 0:
+                worksheet.write(start_row + 2, col, item.get('change_cpl'), cell_format_warning)
+            else:
+                worksheet.write(start_row + 2, col, item.get('change_cpl'), cell_format_total)
+            col += 1
+            worksheet.write(start_row, col, item.get('p1_leads'), cell_format)
+            worksheet.write(start_row + 1, col, item.get('p2_leads'), cell_format)
+            if item.get('change_leads') < 0:
+                worksheet.write(start_row + 2, col, item.get('change_leads'), cell_format_warning)
+            else:
+                worksheet.write(start_row + 2, col, item.get('change_leads'), cell_format_total)
+
+            worksheet.write(start_row + 2, 0, '', cell_format_total)  # fill bg color total row
+            worksheet.write(start_row + 2, 1, '', cell_format_total)  # fill bg color total row
+            start_row += 3
+        return start_row
+
+    def export_excel(self, workbook, worksheet, items, title, start_row=3, skip_row=3):
+        col = 0
+        if len(items) > 0:
+            keys = items[0].keys()
+        else:
+            ValueError('list not items')
+        skip_row = 3  # Отступ
+        width = len(keys)
+        title_format = workbook.add_format({
+            'bold': False,
+            'font_color': 'black',
+            'border': True,
+            'bg_color': 'white',
+            'align': 'center_across',
+            'font_size': 24,
+            'font_name': 'calibri'
+            }
+        )
+
+        worksheet.merge_range(start_row - 2, col, start_row - 1, col + 4, title, title_format)
+        start_row += 2
+        for key in keys:
+            cell_format = workbook.add_format({
+                'bold': True,
+                'font_color': 'black',
+                'border': True,
+                'bg_color': 'd7f2f5',
+                'align': 'center_across',
+                'font_size': 12,
+                'font_name': 'calibri'
+                }
+            )
+            worksheet.write(start_row, col, key, cell_format)
+            col += 1
+        start_row += 1
+
+        for item in items:
+            col = 0
+            for key in keys:
+                cell_format = workbook.add_format({
+                    'bold': False,
+                    'font_color': 'black',
+                    'border': True,
+                    'bg_color': 'white',
+                    'align': 'center_across',
+                    'font_size': 8,
+                    'font_name': 'calibri'
+                }
+                )
+                worksheet.write(start_row, col, item[key], cell_format)
+                col += 1
+            start_row += 1
+        start_row += skip_row
+        return start_row
+
+    def export_excel_direction(self, workbook, worksheet, items, title, start_row=3, skip_row=3):
+        col = 0
+        keys = []
+        items = items.fillna(0)
+        title_format = workbook.add_format({
+            'bold': False,
+            'font_color': 'black',
+            'border': True,
+            'bg_color': 'white',
+            'align': 'center_across',
+            'font_size': 24,
+            'font_name': 'calibri'
+            }
+        )
+
+        value_format = workbook.add_format({
+            'bold': False,
+            'font_color': 'black',
+            'border': True,
+            'bg_color': 'white',
+            'align': 'center_across',
+            'font_size': 8,
+            'font_name': 'calibri'
+            }
+        )
+        for key in items.columns.tolist():
+            column_format = workbook.add_format({
+                'bold': True,
+                'font_color': 'black',
+                'border': True,
+                'bg_color': 'd7f2f5',
+                'align': 'center_across',
+                'font_size': 12,
+                'font_name': 'calibri'
+                }
+            )
+            keys.append(key[0])
+        sub_column_format = workbook.add_format({
+            'bold': True,
+            'font_color': 'black',
+            'border': True,
+            'bg_color': 'e9f2ec',
+            'align': 'center_across',
+            'font_size': 12,
+            'font_name': 'calibri'
+            }
+        )
+
+        keys = set(keys)
+        worksheet.merge_range(start_row - 2, col, start_row - 1, col + 4, title, title_format)
+        start_row += 2
+        worksheet.write(start_row, col, 'Date', column_format)
+        for key in keys:
+            row = start_row
+            worksheet.merge_range(row, col + 1, row, col + 2, key, column_format)
+            worksheet.write(row + 1, col + 1, 'Cost', sub_column_format)
+            worksheet.write(row + 1, col + 2, 'Leads', sub_column_format)
+            for cost_, leads, date in zip(
+                    items[key]['cost_'].tolist(), items[key]['leads'].tolist(), items[key]['cost_'].index):
+                worksheet.write(row + 2, 0, date, value_format)
+                worksheet.write(row + 2, col + 1, cost_, value_format)
+                worksheet.write(row + 2, col + 2, leads, value_format)
+                worksheet.set_column(0, col + 2, 20)
+                row += 1
+            col += 2
+        start_row = row + len(items.index)
+        return start_row
+
+
+
+
     def render_to_response(self, context, **response_kwargs):
         if self.request.GET.get('export') == str(1):
-            with BytesIO() as b:
-                keys = [
-                    {'name': 'Общая статистика', 'key': 'report_client_cabinet'},
-                    {'name': 'Статистика по каналам', 'key': 'report_client_channel'},
-                    {'name': 'Статистика по кампаниям', 'key': 'report_client_campaign'},
-                    {'name': 'Статистика по направлениям ', 'key': 'report_direction_for_export'},
-                    {'name': 'Статистика по периодам', 'key': 'report_client_period_campaign'},
-                    {'name': 'Статистика "Comagic other" ', 'key': 'comagic_other_report'},
-                ]
-                writer = pd.ExcelWriter(b, engine='xlsxwriter')
-                start_row = 3
-                for key in keys:
-                    if context.get(key['key']) is None:
-                        continue
-                    df = pd.DataFrame(context[key['key']])
-                    df.to_excel(writer, startrow=start_row)
-                    workbook = writer.book
-                    worksheet = writer.sheets['Sheet1']
-                    worksheet.set_column(0, 99, 15)
-                    worksheet.set_column('D:D', 55)
-                    merge_format = workbook.add_format({
-                        'align': 'left',
-                        'valign': 'vcenter',
-                        'font_size': 24
-                    })
-                    worksheet.merge_range(start_row - 1, 0, start_row - 2, len(df.columns) - 1,
-                                          key['name'],
-                                          merge_format)
-                    start_row += len(df.index) + 8
-                writer.save()
-                response = HttpResponse(b.getvalue(), content_type='application/vnd.ms-excel')
-                response['Content-Disposition'] = 'attachment; filename="teams.xlsx"'
-                return response
+            response = HttpResponse(content_type='application/vnd.ms-excel')
+            workbook = xlsxwriter.Workbook(response)
+            worksheet = workbook.add_worksheet()
+            row = self.export_excel(workbook, worksheet, context['report_client_cabinet'], 'Общая статистика')
+            row = self.export_excel(workbook, worksheet, context['report_client_channel'], 'Статистика по каналам', row)
+            row = self.export_excel(workbook, worksheet, context['report_client_campaign'], 'Статистика по кампаниям', row)
+            row = self.export_excel_direction(workbook, worksheet, context['report_direction_for_export'], 'Статистика по направлениям', row)
+
+            row = self.export_excel(workbook, worksheet, context['comagic_other_report'], 'Статистика "Comagic other"', row)
+            row = self.export_excel_period(workbook, worksheet, context['report_client_period_campaign'], 'Статистика по периодам', row)
+            workbook.close()
+            response['Content-Disposition'] = f'attachment; filename="{self.object.name}.xlsx"'
+            return response
         else:
             return super().render_to_response(context, **response_kwargs)
 
